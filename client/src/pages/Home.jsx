5632e245389b83e19f263f076319ef8e3333bb6c
darkghost33 <deveshmukherjee76@gmail.com>
2023-02-08 03:18:56 +0530

    hosted backend online on render

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
+      const response = await fetch("https://mern-stack-ai-image-generation-platform.onrender.com/api/v1/post", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
